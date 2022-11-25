083760266841eca9561f167b7bd8428842a731d2
byeongjeon jeon <pensieve92@gmail.com>
2022-11-25 16:25:44 +0900

    Todo overflow 내려오게 하기

--- lib/main.dart
+++ lib/main.dart
@@ -10,50 +10,100 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home:Text('안녕')
-      // home:Icon(Icons.star)
-      // home:Image.asset('assets/dogi.jpg')
-      // home: Center(
-      //   child: Container( width: 50, height: 10, color: Colors.greenAccent),
-      // )
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Daily'),
-        ),
-        body: Align(
-          alignment: Alignment.centerLeft,
-          child: Container(
-            width: double.infinity, height: 150,
-            // margin: EdgeInsets.all(20),
-            margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.red)
-            ),
+        home: Scaffold(
+      appBar: AppBar(
+        // leading: DropdownButton(
+        //   value: _seletected,
+        //   items: ['test', 'test1'],
+        // ),
+        actions: [
+          Icon(Icons.search),
+          Icon(Icons.menu),
+          Icon(Icons.add_alert_outlined)
+        ],
+      ),
+      body: Container(
+        color: Colors.red,
+        alignment: Alignment.topCenter,
+        child: Container(
+            color: Colors.lightBlue,
             padding: EdgeInsets.all(20),
-
-            child: Text('day'),
-          ),
-        ),
-
-        bottomNavigationBar: BottomAppBar(
+            child: Container(
+              color: Colors.yellow,
+              height: 110,
+              alignment: Alignment.topLeft,
+              child: Row(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/dogi.jpg",
+                        width: 150,
+                        // alignment: Alignment.topCenter,
+                      )
+                    ],
+                  ),
+                  Column(
+                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                    Container(
+                      width: 200,
+alignment: Alignment.centerLeft,
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          // TODO overflow 내려오게 하기
+                          Text('data', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20)),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 200,
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text('data', style: TextStyle(color: Colors.grey),),
+                        ],
+                      ),
+                    ),
+                    Container(
+                        width: 200,
+                      alignment: Alignment.centerLeft,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text('data'),
+                          ],
+                        )),
+                    Container(
+                      color: Colors.orange,
+                      width: 200,
+                      alignment: Alignment.centerRight,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children : [Column(children: [Icon(Icons.heart_broken), ],), Column(children: [Text('4')],)]),)
+                  ])
+                ],
+              ),
+            )),
+      ),
+      bottomNavigationBar: BottomAppBar(
           child: SizedBox(
-            height: 50,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page)
-              ],
-            ),
-          )
-
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.phone),
+            Icon(Icons.message),
+            Icon(Icons.contact_page)
+          ],
         ),
-      )
-    );
+      )),
+    ));
   }
 }
-
-
-
-
