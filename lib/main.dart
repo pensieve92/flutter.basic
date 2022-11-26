90adf703b832ed4341599653483c366eae3ce938
byeonghyeon.jeon <pensieve92@gmail.com>
2022-11-26 09:51:33 +0900

    Refactor use Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        // leading: DropdownButton(
-        //   value: _seletected,
-        //   items: ['test', 'test1'],
-        // ),
         actions: [
           Icon(Icons.search),
           Icon(Icons.menu),
@@ -23,75 +19,50 @@ class MyApp extends StatelessWidget {
         ],
       ),
       body: Container(
-        color: Colors.red,
-        alignment: Alignment.topCenter,
-        child: Container(
-            color: Colors.lightBlue,
-            padding: EdgeInsets.all(20),
-            child: Container(
-              color: Colors.yellow,
-              height: 110,
-              alignment: Alignment.topLeft,
-              child: Row(
+          height: 150,
+          color: Colors.red,
+          child: Row(children: [
+            SizedBox(
+              width: 150,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        "assets/dogi.jpg",
-                        width: 150,
-                        // alignment: Alignment.topCenter,
-                      )
-                    ],
-                  ),
-                  Column(
-                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                    Container(
-                      width: 200,
-alignment: Alignment.centerLeft,
-                      color: Colors.white,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          // TODO overflow 내려오게 하기
-                          Text('data', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20)),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      width: 200,
-                      alignment: Alignment.centerLeft,
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text('data', style: TextStyle(color: Colors.grey),),
-                        ],
-                      ),
-                    ),
-                    Container(
-                        width: 200,
-                      alignment: Alignment.centerLeft,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text('data'),
-                          ],
-                        )),
-                    Container(
-                      color: Colors.orange,
-                      width: 200,
-                      alignment: Alignment.centerRight,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children : [Column(children: [Icon(Icons.heart_broken), ],), Column(children: [Text('4')],)]),)
-                  ])
+                  Image.asset(
+                    "assets/dogi.jpg",
+                    width: 150,
+                  )
                 ],
               ),
-            )),
-      ),
+            ),
+            Expanded(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // TODO overflow 내려오게 하기
+                    Text(
+                      'data',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
+                    ),
+                    Text('data', style: TextStyle(color: Colors.grey)),
+                    Text('data'),
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            Icon(Icons.heart_broken),
+                          ],
+                        ),
+                        Column(
+                          children: [Text('4')],
+                        )
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.end,
+                    )
+                  ]),
+            )
+          ])),
       bottomNavigationBar: BottomAppBar(
           child: SizedBox(
         height: 50,
